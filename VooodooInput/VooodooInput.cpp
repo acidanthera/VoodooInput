8e31f751e489125b767c99c4517eb6ee8ce0b7e5
Kishor Prins <me@68x.ca>
2019-07-20 16:03:33 -0400

    Added base IOKit driver

--- VooodooInput/VooodooInput.cpp
+++ VooodooInput/VooodooInput.cpp
@@ -1,1 +1,21 @@
-/* add your code here */
+#include "VooodooInput.hpp"
+
+#define super IOService
+
+bool VoodooInput::start(IOService *provider) {
+    if (!super::start(provider)) {
+        return false;
+    }
+    
+    return true;
+}
+
+void VoodooInput::stop(IOService *device) {
+    super::stop(device);
+}
+
+IOReturn VoodooInput::message(UInt32 type, IOService *provider, void *argument) {
+    IOReturn retValue = super::message(type, provider, argument);
+    
+    return retValue;
+}
--- VooodooInput/VooodooInput.hpp
+++ VooodooInput/VooodooInput.hpp
@@ -1,1 +1,17 @@
-/* add your code here */
+#ifndef VOODOO_INPUT_H
+#define VOODOO_INPUT_H
+
+#include <IOKit/IOService.h>
+
+class VoodooInput : public IOService {
+    OSDeclareDefaultStructors(VoodooInput);
+    
+    IOService* provider;
+public:
+    bool start(IOService* provider) override;
+    void stop(IOService* device) override;
+    
+    IOReturn message(UInt32 type, IOService *provider, void *argument) override;
+};
+
+#endif
